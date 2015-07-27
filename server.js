3cf433d298110bc94c9108ee3d82d4b5c99a049b
Brandon <leebran@onid.oregonstate.edu>
2015-07-27 00:18:50 -0700

    Got hello world up

--- server.js
+++ server.js
@@ -4,3 +4,34 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
+// Configure app to use bodyParser() - allows us to get data from POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set our port
+var port = process.env.PORT || 8080;
+
+
+// Routes for API
+// ---------
+
+// Get instance of express router
+var router = express.Router();
+
+// Test
+router.get('/', function(req, res) {
+    res.json({message: "Hello World"});
+});
+
+// Add additional routes here
+
+// Register routes - all routes will be prefixed with /api
+// --------
+app.use('/api', router);
+
+// Start server
+// --------
+app.listen(port);
+console.log("Server has started on port " + port);
+
